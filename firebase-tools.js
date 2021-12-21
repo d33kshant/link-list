38fe691cb9602373c78e4fad20cff9eba9f8ea1a
Deekshant Yadav <d33kshant@gmail.com>
2021-12-21 19:18:19 +0530

    Updated firebase configs

--- firebase-tools.js
+++ firebase-tools.js
@@ -1,9 +1,17 @@
-import firebase from "firebase";
-import config from "./firebase-config"
+import firebase from "firebase"
+
+const config = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID
+}
 
 const app = !firebase.apps.length ? firebase.initializeApp(config) : firebase.app()
 const database = app.firestore()
 const auth = app.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 
-export {database, auth, provider}
+export { database, auth, provider }
